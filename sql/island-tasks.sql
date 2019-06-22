4b41ccf62953312b48fd557d815bbfa8c10bf3d9
Alessandro Aiezza II <a.aiezza@gmail.com>
2019-06-22 13:20:36 -0500

    Alter Coordinate indexes & add helpful SQL (#2)

--- sql/island-tasks.sql
+++ sql/island-tasks.sql
@@ -1,10 +1,35 @@
-.width 2 %
+.width 2 2 2 %
 SELECT
     `Location`.`coordinate`,
-    COUNT(`HeartPiece`.`id`)
-    -- , CASE WHEN `HeartPiece`.`task` IS NOT NULL THEN COUNT(`HeartPiece`.`id`) ELSE NULL END
-    AS "Heart Piece"
+    SUBSTR('00' || `HeartPieceLocation`.`HeartPieces`, -2, 2) AS "HP",
+    SUBSTR('00' || `TreasureChartLocation`.`TreasureCharts`, -2, 2) AS "TC"
+
 FROM (SELECT `Latitude`.`value`||`Longitude`.`value` AS "coordinate"
-    FROM `Latitude` CROSS JOIN `Longitude`) AS "Location" LEFT JOIN `HeartPiece` ON
+    FROM `Latitude` CROSS JOIN `Longitude`) AS "Location"
+
+LEFT JOIN (SELECT
+        `Location`.`coordinate`,
+        CASE WHEN `HeartPiece`.`id` IS NOT NULL THEN
+            COUNT(`HeartPiece`.`id`) ELSE NULL END AS "HeartPieces"
+    FROM (SELECT `Latitude`.`value`||`Longitude`.`value` AS "coordinate"
+        FROM `Latitude` CROSS JOIN `Longitude`) AS "Location"
+    LEFT JOIN`HeartPiece` ON
         `Location`.`coordinate` = `HeartPiece`.`latitude`||`HeartPiece`.`longitude`
-GROUP BY `Location`.`coordinate`;
+    GROUP BY
+        `Location`.`coordinate`) AS `HeartPieceLocation` ON
+    `Location`.`coordinate` = `HeartPieceLocation`.`coordinate`
+
+LEFT JOIN (SELECT
+        `Location`.`coordinate`,
+        CASE WHEN `TreasureChart`.`number` IS NOT NULL THEN
+            COUNT(`TreasureChart`.`number`) ELSE NULL END AS "TreasureCharts"
+    FROM (SELECT `Latitude`.`value`||`Longitude`.`value` AS "coordinate"
+        FROM `Latitude` CROSS JOIN `Longitude`) AS "Location"
+    LEFT JOIN`TreasureChart` ON
+        `Location`.`coordinate` = `TreasureChart`.`latitude`||`TreasureChart`.`longitude`
+    GROUP BY
+        `Location`.`coordinate`) AS `TreasureChartLocation` ON
+    `Location`.`coordinate` = `TreasureChartLocation`.`coordinate`
+
+GROUP BY
+    `Location`.`coordinate`;
